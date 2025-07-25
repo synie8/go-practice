e0df51065190f8a8c2420020f88cf83d7e845bdc
synie <synie@isoftstone.com>
2025-07-25 13:52:51 +0800

    任务打卡 136. 只出现一次的数字

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{4, 1, 2, 1, 2}
+	fmt.Println("第一种me ******", singleNumber(a))
+}
--- palindrome.go
+++ palindrome.go
@@ -1,3 +1,4 @@
+//package palindrome
 package main
 
 import (
@@ -45,9 +46,4 @@ func isPalindrome2(x int) bool {
 	// 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
 	// 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
 	return x == revertedNumber || x == revertedNumber/10
-}
-
-func main() {
-	fmt.Println("第一种me ******", isPalindrome(121))
-	fmt.Println("第二种 ******", isPalindrome2(12321))
 }
